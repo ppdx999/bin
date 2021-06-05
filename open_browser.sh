f3ca5761846e4031bbcbf4c13a80140a1718c149
ppdx999 <ppdx999@gmail.com>
2021-06-05 21:31:01 +0900

    Modify it. Enable to path multiple argument and path them thorugh pipeline.

--- open_browser.sh
+++ open_browser.sh
@@ -3,10 +3,10 @@
 # @(#) hoge.sh ver.1.0.0 2008.04.24
 #
 # Usage:
-#   $0 [-a] [-b] [-f filename] arg1 ...
+#   $0 ...
 # 
 # Description:
-#   hogehogehoge
+#   Opne file in chrome browser. 
 # 
 # Options:
 #   -a    aaaaaaaaaa
@@ -17,22 +17,22 @@
 
 _main(){
 
-	[ ! -f ~/lib/plantuml/plantuml.jar ] && error_exit "plantuml.jar cannot be found. Please add it to \"~/lib/plantuml/plantuml.jar\""
-
 	for arg in "$@"
 	do
 		if [ ! -f "$arg" ]; then
-			echo "$arg"" could not be found" 1>&2
+			echo "$arg"" could not be found" 1&>2
 			continue
 		fi
-		java -jar ~/lib/plantuml/plantuml.jar -charset UTF-8 "$arg"
+
+		dir=$(dirname "$arg")
+		file=$(basename "$arg")
+
+		if [ "$(expr substr $(uname -s) 1 10)" == 'MINGW64_NT' ]; then
+			cd "$dir"
+			start chrome "$PWD"/"$file"
+		fi
 	done
 
-}
-
-error_exit(){
-	echo "$0: ${1:-"Unknown Error"}" 1>&2
-	exit 1
 }
 
 if [ -p /dev/stdin ]; then
